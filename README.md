4b858c6e9bfbb7c4511449bbb7255a06048a860e
Tobias Brox <tobias@redpill-linpro.com>
2025-05-28 19:14:08 +0200

    refactoring and tweaing algorithms

--- README.md
+++ README.md
@@ -25,7 +25,7 @@ Briefly, this tool can do two things:
 * More advanced categorization algorithms than what can readily be found in Activitywatch
 * Auto-exporting activities from Activitywatch to Timewarrior
 
-### Categorization algorithms
+### Categorization rules
 
 This should probably be moved out to a separate tool, but currently it's a bit hard-coded to Timewarrior.  Timewarrior don't use terms like categories, projects, tasks etc - it's all about tags, and up to the user to figure out how to use the tags, hence the categorization is tag-based.  The current implementation also stores intermediate data in timew, picks it out and expands on it (ugly design, should be redone).  Rules are currently stored in `$HOME/.config/activitywatch/aw-export-timewarrior/aw-export-timewarrior.toml`.  Some fallback-rules on things that can be ignored are currently hard-coded in the script, but should be moved to the config.
 
@@ -43,13 +43,30 @@ The three latter takes the parameter `timew_tags` and will start a new Timewarri
 
 I'm planning to support extensions - for instance, you may want to write up some special browser rule logic applying to some specific URL and specific page title, do an external look-up to find the appropriate tags, etc - advanced logic is better done in Python than in a configuration language like toml.
 
+### "Stickyness" and tuning
+
+This is difficult!
+
+* Very short visits to windows should be completely ignored.
+* Some "noise" should be ignored.  You don't want this script too frequently changing your activity status if you've set it manually.
+* There will be lots of events that cannot easily be categorized, i.e. work on the command line - unless we have strong hints on something else, we should assume you're still working on the same.
+* Sometimes a work task involves frequent switching between windows - tags from both windows should apply in such situations.  Meaning we have to store tags internally for a while before dumping them to timewarrior.
+
+Proposal:
+
+* Very short visits to windows should be completely ignored.  You didn't really do anything in the window, just searching for the right window for your work, or the mouse coursor accidentally went over some window (`IGNORE_INTERVAL=5` seconds by default)
+* `MIN_RECORDING_INTERVAL=60` seconds is the very minimum time between each distinct entry recorded in timewarror.
+* Whenever some tags have been observed for accumulatively more than `MIN_RECORDING_INTERVAL`, we should consider to start some new activity.  All tags observed for more than `MIN_TAG_INTERVAL=30` seconds over the last open period applies.  If any of those tags are missing from the current activity tracking, `timew start` is appliced with the new list of tags.  (TODO: consider that some tags should not be combined in the same event)
+* We should have some stickyness - the records should not be completely reset when sending tags to timew.  Rather, multiply all existing tags in the accumulator with `STICKINESS_FACTOR=0.25` (Considering removing it - it breaks the requirement that the script should always to the same if doing `timew undo` an arbitrary number of times)
+* If `timew start` has been run manually, the accumulator should be reset and populated with tags from the manual run.
+* If a single window event lasts for more than, say, `MAX_MIXED_INTERVAL=300` seconds and can be identified with tags, then ignore all previous activity and treat it independently (if you did something quickly between two well-defined tasks, then that time will be attributed to the previous work task).
+
 ### Export algorithm
 
 Currently it does support rewriting old Timewarrior history.  The export algorithm works like this:
 
 * The starting time for the current activity in Timewarrior is used as a starting point (Timewarrior gaps are not supposed - *all* the time should be tracked).  It's currently not possible to use the exporter to rewrite the Timewarrior history (but it could be done).
 * At three points of the code the retagging-algorithm kicks in - it's at startup, after starting a new activity, and before checking if it's relevant to start a new activity.  The retagging-algorithm will read retagging rules from `config['tags']` and add more tags if the configuration fits the tags given.
-* Events shorter than 20s (should be made configurable) is generally ignored.  Exception is after 120s or more has passed without any events leading to taggings - then any hints of what you're doing will be considered. 
 * All window events and afk-events will be checked.  For window-events coming from a browser, the corresponding browser-event (if any) will be looked up, and the URL checked towards the rules given.  Sort of the same with editor events.  Other events will be matched towards app-rules.
 * If any of the tags that are found by the rules are not included in the current tracking, it's considered that a new activity have started, and `timew` will be invoked with the new tags and the timestamp from the event.  Duration is (as for now) ignored (and for good reasons, one may frequently switch i.e. between a browser, terminal windows and an editor while working).
 
--- src/aw_export_timewarrior/main.py
+++ src/aw_export_timewarrior/main.py
@@ -7,14 +7,22 @@ from datetime import datetime, timezone, timedelta
 from collections import defaultdict
 from termcolor import cprint
 import re
+import os
 
 from .config import config
 
-## warning threshold for outdated data (TODO: move to the config file, give a better name?)
-WARN_THRESHOLD=300
-SLEEP_INTERVAL=120
-MINIMUM_INTERVAL=25
-MAXIMUM_NONCAT_INTERVAL=300
+## Those should be moved to config.py before releasing.  Possibly renamed.
+AW_WARN_THRESHOLD=300 ## the recent data from activity watch should not be elder than this
+SLEEP_INTERVAL=3 ## Sleeps between each run when the program is run in real-time
+IGNORE_INTERVAL=5 ## ignore any window visits lasting for less than five seconds (TODO: may need tuning if terminal windows change title for every command run)
+MIN_RECORDING_INTERVAL=60 ## Never record an activities more frequently than once per minute.
+MIN_TAG_RECORDING_INTERVAL=30 ## When recording something, include every tag that has been observed for more than 30s
+STICKYNESS_FACTOR=0.25 ## Don't reset everything on each "tick".
+MAX_MIXED_INTERVAL=180 ## Any window event lasting for more than three minutes should be considered independently from what you did before and after
+
+GRACE_TIME=float(os.environ.get('GRACE_TIME') or 10)
+
+SPECIAL_TAGS={'manual', 'override', 'not-afk'}
 
 def ts2str(ts, format="%FT%H:%M"):
     return(ts.astimezone().strftime(format))
@@ -48,6 +56,8 @@ class Exporter:
 
     def get_editor_tags(self, window_event):
         ## TODO: can we consolidate common code? I basically copied get_browser_tags and s/browser/editor/ and a little bit editing
+        if window_event['data'].get('app', '').lower() not in ('emacs', 'vi', 'vim', ...): ## TODO - complete the list
+            return False
         editor = window_event['data']['app'].lower()
         editor_id = self.bucket_short[f"aw-watcher-{editor}"]['id']
         editor_events = self.aw.get_events(editor_id, start=window_event['timestamp'], end=window_event['timestamp']+window_event['duration'])
@@ -67,23 +77,29 @@ class Exporter:
             rule = config['rules']['editor'][editor_rule_name]
             for project in rule.get('projects', []):
                 if(project == editor_event['data']['project']):
-                    return rule['timew_tags'] + [ 'not-afk' ]
+                    ret = set(rule['timew_tags'])
+                    ret.add('not-afk')
+                    return ret
             if 'path_regexp' in rule:
                 match = re.search(rule['path_regexp'], editor_event['data']['file'])
                 if match:
-                    tags = rule['timew_tags']
+                    tags = set(rule['timew_tags'])
                     for tag in tags:
                         if '$1' in tag:
                             ## todo: support for $2, etc
                             tags.remove(tag)
-                            tags.append(tag.replace('$1', match.group(1)))
-                    return tags  + [ 'not-afk' ]
+                            tags.add(tag.replace('$1', match.group(1)))
+                    tags.add('not-afk')
+                    return tags
                 
         self.log(f"Unhandled editor event.  File: {editor_event['data']['file']}", window_event['timestamp'], attrs=["bold"])
         return []
         
 
     def get_browser_tags(self, window_event):
+        if not window_event['data'].get('app', '').lower() in ('chromium', 'firefox', 'chrome', ...): ## TODO - complete the list
+            return False
+
         browser = window_event['data']['app'].lower().replace('chromium', 'chrome')
         browser_id = self.bucket_short[f"aw-watcher-web-{browser}"]['id']
         browser_events = self.aw.get_events(browser_id, start=window_event['timestamp'], end=window_event['timestamp']+window_event['duration'])
@@ -101,16 +117,17 @@ class Exporter:
             if 'url_regexp' in rule:
                 match = re.search(rule['url_regexp'], browser_event['data']['url'])
                 if match:
-                    tags = rule['timew_tags']
+                    tags = set(rule['timew_tags'])
                     for tag in tags:
                         if '$1' in tag:
                             ## todo: support for $2, etc
                             tags.remove(tag)
                             try:
-                                tags.append(tag.replace('$1', match.group(1)))
+                                tags.add(tag.replace('$1', match.group(1)))
                             except IndexError:
                                 pass
-                    return tags  + [ 'not-afk' ]
+                    tags.add('not-afk')
+                    return tags
         if browser_event['data']['url'] in ('chrome://newtab/', 'about:newtab'):
             return []
         self.log(f"Unhandled browser event.  URL: {browser_event['data']['url']}", window_event['timestamp'], attrs=["bold"])
@@ -141,18 +158,37 @@ class Exporter:
                         tags.add(tag)
                 tags.add('not-afk')
                 return tags
-        return []
+        return False
 
+
+    def get_afk_tags(self, event):
+        if 'status' in event['data']:
+            return { event['data']['status'] }
+        else:
+            return False
+
+    def find_tags_from_event(self, event):
+        if event['duration'].total_seconds() < IGNORE_INTERVAL:
+            return
+
+        for method in (self.get_app_tags, self.get_browser_tags, self.get_afk_tags, self.get_editor_tags):
+            tags = method(event)
+            if tags is not False:
+                break
+        return tags
+    
     def find_next_activity(self):
         afk_id = self.bucket_by_client['aw-watcher-afk'][0]
         window_id = self.bucket_by_client['aw-watcher-window'][0]
+        tags_accumulated_time = defaultdict(timedelta)
 
         afk_events = self.aw.get_events(afk_id, start=self.last_tick)
+        ### START WORKAROUND
         ## TODO
         ## workaround for https://github.com/ActivityWatch/aw-watcher-window-wayland/issues/41
         ## assume all gaps are afk
         if len(afk_events) == 0:
-            afk_events = [{'data': {'status': 'afk'}, 'timestamp': self.last_tick, 'duration': timedelta(seconds=time.time()-self.last_tick)}]
+            afk_events = [{'data': {'status': 'afk'}, 'timestamp': self.last_tick, 'duration': timedelta(seconds=time()-self.last_tick.timestamp())}]
         elif len(afk_events)>1: #and not any(x for x in afk_events if x['data']['status'] != 'not-afk'):
             afk_events.reverse()
             afk_events.sort(key=lambda x: x['timestamp'])
@@ -160,33 +196,15 @@ class Exporter:
                 end = afk_events[i]['timestamp']
                 start = afk_events[i-1]['timestamp'] + afk_events[i-1]['duration']
                 duration = end-start
-                if duration < timedelta(seconds=MINIMUM_INTERVAL):
+                if duration.total_seconds() < MIN_RECORDING_INTERVAL:
                     continue
                 afk_events.append({'data': {'status': 'afk'}, 'timestamp': start, 'duration': duration})
-                
+        ### END WORKAROUND
         afk_window_events = self.aw.get_events(window_id, start=self.last_tick) + afk_events
         afk_window_events.sort(key=lambda x: x['timestamp'])
         for event in afk_window_events:
 
-            ## TODO - refactor better, this is a bit ugly, and references to timew should be abstracted out
-            
-            ## Refactoring idea:
-            ## MINIMUM_INTERVAL to be set very short.  Truly ignore only windows that you've been touching.
-            ##
-            ## stage one is to find the tags.
-            ## perhaps like this:
-            ##   for method in ...: tags=method() ; if tags: continue
-            ##
-            ## stage two is to deal with the tags
-            ## tag rewritings to be done (without hitting "timew start")
-            ## if the duration of the event is lower than the MAXIMUM_NONCAT_INTERVAL, put tags into an accumulating table
-            ## if the duration of the event exceeds the MAXIMUM_NONCAT_INTERVAL, set tags at once with timew_ensure and reset the accumulating table
-            ## if the accumulated time exceeds MAXMIMUM_NONCAT_INTERVAL, compare the accumulating table with the tags that are running.
-            ## We need a third threshold, a ratio, perhaps 0.5?  So any tags seen more than half of the time over the last MAXIMIMUM_NONCAT_INTERVAL window will be considered.
-            ## Running tags should have some stickyness.  Perhaps add all the running tags with 0.4*MAXIMUM_NONCAT_INTERVAL, so that if you visit any window related to the
-            ## existing activity, the tags will be kept.
-            ## We should consider that some tags may exclude each other, otherwise it's easy to track more than 24 hours per day. "Frem med faktureringsgaffelen!" we'd say in Norwegian.
-            def timew_ensure(tags):
+            def timew_ensure(tags, since=event['timestamp']):
                 if tags != 'not-afk':
                     self.last_known_tick = event['timestamp'] + event['duration']
                 self.timew_info = timew_retag(get_timew_info())
@@ -198,40 +216,58 @@ class Exporter:
                     return
                 if set(tags).issubset(self.timew_info['tags']):
                     return
-                timew_run(['start'] + list(tags) + [event['timestamp'].astimezone().strftime('%FT%H:%M:%S')])
+                tags = retag_by_rules(tags)
+                assert not exclusive_overlapping(tags)
+                timew_run(['start'] + list(tags) + [since.astimezone().strftime('%FT%H:%M:%S')])
                 self.timew_info = timew_retag(get_timew_info())
 
+            tags = self.find_tags_from_event(event)
 
-            ## "overdue" means the current recorded activity may have stopped "long" ago.
-            if 'manual' in self.timew_info['tags']:
-                overdue = False
-            else:
-                overdue = event['timestamp'] + event['duration'] - self.last_known_tick > timedelta(seconds=MAXIMUM_NONCAT_INTERVAL)
+            print(f"{self.last_tick.astimezone().strftime("%H:%M")} - {event['duration'].total_seconds()}s duration, {event['data']} - tags found: {tags}")
+            
+            if tags == { 'not-afk' }:
+                self.last_known_tick = event['timestamp']
+                tags = []
 
-            ## TODO: consider the minimum interval of events yielding the same tags,
-            ## so that i.e. frequent switching between editor and browser would not be ignored.
-            if event['duration'].total_seconds() < MINIMUM_INTERVAL and not overdue:
-                continue
-            elif event['data'].get('status') == 'afk':
-                if overdue:
-                    timew_ensure('afk')
-            elif event['data'].get('status') == 'not-afk':
-                ## TODO: we should probably have a bit better handling of this one
-                timew_ensure('not-afk')
-            elif self.get_app_tags(event): ## inelegant - running the algorithm twice
-                timew_ensure(self.get_app_tags(event))
-            elif event['data'].get('app', '').lower() in ('chromium', 'firefox', 'chrome'):
-                timew_ensure(self.get_browser_tags(event))
-            elif event['data'].get('app', '').lower() in ('emacs', 'vi', 'vim'): ## todo - complete the list
-                timew_ensure(self.get_editor_tags(event))
+            ## Ref README, if MAX_MIXED_INTERVAL is met, ignore accumulated minor activity
+            ## (the mixed time will be attributed to the previous work task)
+            if tags and event['duration'].total_seconds() > MAX_MIXED_INTERVAL:
+                tags_accumulated_time = defaultdict(timedelta)
+                timew_ensure(tags)
+
+            interval_since_last_known_tick = event['timestamp'] + event['duration'] - self.last_known_tick
+
+            ## Track things in internal accumulator if the focus between windows changes often
+            if tags:
+                tags = retag_by_rules(tags)
+                for tag in tags:
+                    tags_accumulated_time[tag] += event['duration']
+                
+            if interval_since_last_known_tick.total_seconds() > MIN_RECORDING_INTERVAL and any(x for x in tags_accumulated_time if x not in SPECIAL_TAGS and tags_accumulated_time[x].total_seconds()>MIN_RECORDING_INTERVAL):
+                tags = set()
+                assert not exclusive_overlapping(tags)
+                min_tag_recording_interval=MIN_TAG_RECORDING_INTERVAL
+                while exclusive_overlapping(set([tag for tag in tags_accumulated_time if  tags_accumulated_time[tag].total_seconds() > min_tag_recording_interval])):
+                    min_tag_recording_interval += 1
+                for tag in tags_accumulated_time:
+                    if tags_accumulated_time[tag].total_seconds() > min_tag_recording_interval:
+                        tags.add(tag)
+                        if ('oss-contrib' in tags and 'entertainment' in tags):
+                            breakpoint()
+                            exclusive_overlapping(tags)
+                    tags_accumulated_time[tag] *= STICKYNESS_FACTOR
+                ## TODO: if `timew start` was run manually, then repopulate tags_accumulated_time to ensure sticiness of manually recorded tags
+                timew_ensure(tags, self.last_known_tick)
+
+            self.last_tick = event['timestamp']-timedelta(seconds=1)
+
+            if tags is not False:
+                pass
+            
             elif event['data'].get('app', '').lower() in ('foot', 'xterm', ...): ## TODO: complete the list
                 self.log(f"Unknown terminal event {event['data']['title']}", event['timestamp'], attrs=["bold"])
-                if overdue:
-                    timew_ensure({'unknown', 'not-afk'}) ## is this smart?
             else:
                 self.log(f"No rules found for event {event['data']}", event['timestamp'], attrs=["bold"])
-            self.last_tick = event['timestamp']-timedelta(seconds=1)
-            print(f"{self.last_tick.astimezone().strftime("%H:%M")} - {event['data']}")
 
     def tick(self):
         self.timew_info = timew_retag(get_timew_info())
@@ -241,7 +277,7 @@ class Exporter:
         self.find_next_activity()
 
 def check_bucket_updated(bucket: dict):
-    if time()-bucket['last_updated_dt'].timestamp() > WARN_THRESHOLD:
+    if time()-bucket['last_updated_dt'].timestamp() > AW_WARN_THRESHOLD:
         logging.warning(f"Bucket {bucket['id']} seems not to have recent data!")
 
 ## TODO: none of this has anything to do with ActivityWatch and can be moved to a separate module
@@ -259,22 +295,42 @@ def timew_run(commands):
     print("Running:")
     print(f"   {" ".join(commands)}")
     subprocess.run(commands)
-    cprint("Use timew undo if you don't agree!  You have ten seconds to press ctrl^c", attrs=["bold"])
-    sleep(10)
+    cprint(f"Use timew undo if you don't agree!  You have {GRACE_TIME} seconds to press ctrl^c", attrs=["bold"])
+    sleep(GRACE_TIME)
 
-def timew_retag(timew_info):
-    source_tags = set(timew_info['tags'])
+def exclusive_overlapping(tags):
+    for gid in config['exclusive']:
+        group = set(config['exclusive'][gid]['tags'])
+        if len(group.intersection(tags)) > 1:
+            return True
+    return False
+
+## not really retag, more like expand tags?
+def retag_by_rules(source_tags):
+    assert not exclusive_overlapping(source_tags)
     new_tags = source_tags.copy()
     for tag_section in config['tags']:
         retags = config['tags'][tag_section]
         if source_tags.intersection(set(retags['source_tags'])):
-            new_tags = new_tags.union(set(retags.get('add', [])))
+            new_tags_ = new_tags.union(set(retags.get('add', [])))
+            if exclusive_overlapping(new_tags_):
+                logging.warning(f"Excluding expanding tag rule {tag_section} due to exclusivity conflicts")
+            else:
+                new_tags = new_tags_
+    if new_tags != source_tags:
+        ## We could end up doing infinite recursion here
+        ## TODO: add some recursion-safety here?
+        return retag_by_rules(new_tags)
+    return new_tags
 
+def timew_retag(timew_info):
+    source_tags = set(timew_info['tags'])
+    new_tags = retag_by_rules(source_tags)
     if new_tags != source_tags:
         timew_run(["retag"] + list(new_tags))
         timew_info = get_timew_info()
         assert set(timew_info['tags']) == new_tags
-        return timew_retag(timew_info)
+        return timew_info
     return timew_info
 
 def main():
